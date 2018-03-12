a61d1bd49afb42bb69216541589ab499f319c2e0
Mark Hibberd <mark@hibberd.id.au>
2018-03-12 21:45:06 +1100

    Receive replicate from a peer.

--- src/Agent/Remote.hs
+++ src/Agent/Remote.hs
@@ -32,6 +32,23 @@ _replication peers log = do
       Process.send pid (Replicate d)
   pure $ log
 
+-- |
+-- Receive @log@ from a @peer@. This is non blocking. The @Left@ output
+-- is used to indicate that no message was received, the @Right@ output
+-- is used to indicate that a messasge was received. They contain the
+-- same log and can be unified with @either id id@, but keeping track
+-- of the distinction allows downstream to decide how to handle the
+-- different paths.
+_receive :: Log ProcessId -> Process (Either (Log ProcessId) (Log ProcessId))
+_receive log =
+  Process.expectTimeout 0 >>= \message -> case message of
+    Just (Replicate remote) -> do
+      self <- Process.getSelfPid
+      pure . Right $ Log.merge self log remote
+    Nothing ->
+      pure . Left $ log
+
+
 agent :: ProcessId -> Process ()
 agent client =
   Process.expect >>= \message -> case message of
